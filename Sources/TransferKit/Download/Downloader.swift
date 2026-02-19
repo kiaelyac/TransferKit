18571b8af9a502666199d9cc2b3e95fb68aadcb2
K.Elyasi <K.Elyasi@sadad.co.ir>
2026-02-19 19:44:52 +0330

    revoke changes

--- Sources/TransferKit/Download/Downloader.swift
+++ Sources/TransferKit/Download/Downloader.swift
@@ -9,27 +9,25 @@ import Foundation
 
 public class Downloader {
     public init() { }
-    public func download(for request: RequestStructurable) async throws -> AsyncThrowingStream<DownloadModel, Error> {
+    public func download(for request: RequestStructurable) async throws -> AsyncThrowingStream<Data, Error> {
         let urlRequest = try request.asURLRequest()
-        let (asyncBytes, response) = try await URLSession.shared.bytes(for: urlRequest)
+        let (data, response) = try await URLSession.shared.bytes(for: urlRequest)
         
         guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
             throw URLError(.badServerResponse)
         }
-        return AsyncThrowingStream<DownloadModel, Error> { continuation in
+        return AsyncThrowingStream<Data, Error> { continuation in
             Task {
                 let accumulator = ByteAccumulator(size: Int(response.expectedContentLength))
-                var iterator = asyncBytes.makeAsyncIterator()
-                var item: DownloadModel = .init(progress: 0, data: nil)
                 while !accumulator.checkCompleted() {
-                    while !accumulator.isChunkCompleted, let byte = try await iterator.next() {
-                        accumulator.append(byte)
-                        item.data = accumulator.data
-                        item.progress = accumulator.progress
-                        continuation.yield(item)
+                    while !accumulator.isChunkCompleted {
+                        for try await byte in data {
+                            accumulator.append(byte)
+                            continuation.yield(accumulator.data)
+                        }
                     }
                 }
-                continuation.finish()
+                continuation.yield(accumulator.data)
             }
         }
     }
